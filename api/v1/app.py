2b07f90640b890d536130ad510bd58a1e6b6c30c
dshikuta <dshikuta@gmail.com>
2023-05-02 13:39:55 -0700

    storage brand

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,7 +8,6 @@ from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 
-
 app = Flask(__name__)
 '''The Flask web application instance.'''
 app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
@@ -17,19 +16,16 @@ app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
 CORS(app, resources={'/*': {'origins': app_host}})
 
-
 @app.teardown_appcontext
 def teardown_flask(exception):
     '''The Flask app/request context end event listener.'''
     # print(exception)
     storage.close()
 
-
 @app.errorhandler(404)
 def error_404(error):
     '''Handles the 404 HTTP error code.'''
     return jsonify(error='Not found'), 404
-
 
 @app.errorhandler(400)
 def error_400(error):
@@ -38,7 +34,6 @@ def error_400(error):
     if isinstance(error, Exception) and hasattr(error, 'description'):
         msg = error.description
     return jsonify(error=msg), 400
-
 
 if __name__ == '__main__':
     app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
